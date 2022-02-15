1a1bce59f299718d5920db4a896972f38c5567ee
userHWSeo <gmldnjseocnd@naver.com>
2022-02-15 22:01:29 +0900

    0215 update
    requests, responses, middlewares

--- src/server.js
+++ src/server.js
@@ -1,10 +1,35 @@
 import express from "express";
-import { handle } from "express/lib/application";
 
 const PORT = 4000;
 
 const app = express();
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${ PORT } 👍`)
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+        const url = req.url;
+        if(url === "/protected"){
+            return res.send("<h1>Not Allowed</h1>");
+        }
+        console.log("Allowed, you may continue.");
+        next();
+}
+
+const handleHome = (req, res) => {
+    return res.send("hi");
+}
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge !");
+}
+
+app.get("/", logger, handleHome);
+app.get("/protected", privateMiddleware, handleProtected);
+
+const handleListening = () => 
+    console.log(`Server listening on port http://localhost:${PORT} 👍`)
 
 app.listen(PORT, handleListening);
