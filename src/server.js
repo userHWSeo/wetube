d614358e759580b8ad735ada232c123404392fcd
userHWSeo <gmldnjseocnd@naver.com>
2022-02-16 23:38:07 +0900

    0216 update
    learn req, res, next, middleware, morgan

--- src/server.js
+++ src/server.js
@@ -1,35 +1,25 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hi");
+};
 
-const privateMiddleware = (req, res, next) => {
-        const url = req.url;
-        if(url === "/protected"){
-            return res.send("<h1>Not Allowed</h1>");
-        }
-        console.log("Allowed, you may continue.");
-        next();
-}
+const login = (req, res) => {
+  return res.send("login !");
+};
 
-const handleHome = (req, res) => {
-    return res.send("hi");
-}
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge !");
-}
-
-app.get("/", logger, handleHome);
-app.get("/protected", privateMiddleware, handleProtected);
-
-const handleListening = () => 
-    console.log(`Server listening on port http://localhost:${PORT} 👍`)
+const handleListening = () =>
+  console.log(`✔ Server listening on port http://localhost:${PORT} 👍`);
 
 app.listen(PORT, handleListening);
