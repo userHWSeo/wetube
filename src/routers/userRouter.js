6f15caacd1180e676324f0a38afa4e86b3b51be9
userHWSeo <gmldnjseocnd@naver.com>
2022-02-19 19:44:58 +0900

    0219 update
    learn routers and export, parameters

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { edit, remove } from "../controllers/userController";
+import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-const handleEditUser = (req, res) => res.send("Edit User");
-const handleDelete = (req, res) => res.send("Delete User");
-
-userRouter.get("/edit", handleEditUser);
-userRouter.get("/delete", handleDelete);
+userRouter.get("/logout", logout);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
+userRouter.get(":id", see);
 
 export default userRouter;
