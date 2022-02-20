116fd1a712e39f3bb742869c9cb828228ec41738
userHWSeo <gmldnjseocnd@naver.com>
2022-02-20 23:14:24 +0900

    0220 update
    learn pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  res.send("Edit");
-};
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.send("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
