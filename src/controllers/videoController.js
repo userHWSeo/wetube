12905668ae1cc33180444b5138b4c15d7103d61c
userHWSeo <gmldnjseocnd@naver.com>
2022-02-21 18:31:58 +0900

    0221 update
    learn iteration and mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,32 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 3,
+      comment: 2,
+      createdAt: "2 minute ago",
+      views: 31,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comment: 2,
+      createdAt: "10 minute ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 4,
+      comment: 2,
+      createdAt: "3 minute ago",
+      views: 45,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("edit");
 export const search = (req, res) => res.send("Search");
