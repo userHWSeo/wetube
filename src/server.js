7c3e7715152c3591db5d86642795a5f58ecc2a1a
userHWSeo <gmldnjseocnd@naver.com>
2022-02-23 23:28:15 +0900

    0223 update
    practice database

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouters";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
