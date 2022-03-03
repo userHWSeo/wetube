ca73ca68ca91e081c9573161c8dc3510ea5cd15c
userHWSeo <gmldnjseocnd@naver.com>
2022-03-03 20:26:17 +0900

    0303 update
    make login form and learn status codes (200, 400 error, 404 error...)

--- src/models/User.js
+++ src/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  password2: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
 });
