0b475e1baf0c4299d5f1455bbab3f54baf7403ff
userHWSeo <gmldnjseocnd@naver.com>
2022-03-15 23:32:20 +0900

    0315 update
    make video upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -47,16 +47,14 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+  const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
-      meta: {
-        views: 0,
-        rating: 0,
-      },
     });
     return res.redirect("/");
   } catch (error) {
