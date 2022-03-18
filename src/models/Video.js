cb1050651da6b353536afdc000a38145bdeb76bd
userHWSeo <gmldnjseocnd@naver.com>
2022-03-18 23:38:11 +0900

    0318 update
    make edit.pug learn populate and  bugfixed

--- src/models/Video.js
+++ src/models/Video.js
@@ -8,13 +8,14 @@ const videoSchema = new mongoose.Schema({
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
-    rating: { type: Number, default: 0, required: true },
   },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 videoSchema.static("formatHashtags", function (hashtags) {
-  hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
