2215be21d100ed584503abba308f4fb94dfdf86d
userHWSeo <gmldnjseocnd@naver.com>
2022-03-20 23:54:38 +0900

    0320 update
    install & learn & test webpack

--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use(localsMiddleware);
 app.use("/upload", express.static("upload"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
