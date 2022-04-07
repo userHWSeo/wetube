f447ff98e168482f4389eb7be5620608a1eb0fa5
userHWSeo <gmldnjseocnd@naver.com>
2022-04-07 22:10:46 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/Comment";
 import "./models/User";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✔ Server listening on port http://localhost:${PORT} 👍`);
