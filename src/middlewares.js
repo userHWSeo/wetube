cb32cd2ca6079f866187ff7c7253ef155152c2db
userHWSeo <gmldnjseocnd@naver.com>
2022-04-08 17:52:47 +0900

    0408 update
    could not resolve the error

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,6 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
-import s3Storage from "multer-s3";
 
 const s3 = new aws.S3({
   credentials: {
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,5 @@
 import express from "express";
 import {
-  edit,
   getEdit,
   postEdit,
   logout,
