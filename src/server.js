c6f0668242a6d7dd3683536eb9ffa99eef3c3bc3
userHWSeo <gmldnjseocnd@naver.com>
2022-04-08 20:12:43 +0900

    update05

--- src/server.js
+++ src/server.js
@@ -33,12 +33,6 @@ app.use((req, res, next) => {
   });
 });
 
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
-
 app.use(flash());
 app.use(localsMiddleware);
 app.use("/upload", express.static("upload"));
